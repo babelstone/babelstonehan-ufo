9fd0cb29baeab91c0bc61e8bfd4c4ae2adf74fa8
Huáng Jùnliàng <jlhwung@gmail.com>
2025-05-14 10:15:22 -0400

    build: unzip PUA from the beta release

--- download.sh
+++ download.sh
@@ -3,5 +3,3 @@
 curl --remote-name --remote-header-name --location https://www.babelstone.co.uk/Fonts/Download/BabelStoneHanBeta.zip
 unzip -o BabelStoneHanBeta.zip
 rm BabelStoneHanBeta.zip
-
-curl --remote-name --remote-header-name --location https://www.babelstone.co.uk/Fonts/Download/BabelStoneHanPUA.ttf
